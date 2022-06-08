d05215232926a91d989537b185d2105b9042165f
Ryo Ota <ota.ryo.1026@gmail.com>
2022-06-08 21:31:40 +0900

    initial commit.

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,47 @@
+// import { defineUserConfig } from "vuepress-vite";
+// import { defaultTheme } from "vuepress"
+import { defineUserConfig } from "@vuepress/cli";
+import { defaultTheme } from "@vuepress/theme-default";
+import { searchPlugin } from "@vuepress/plugin-search"
+
+export default defineUserConfig({
+    base: '/',
+    locales: {
+        '/': {
+            lang: 'ja',
+            title: 'No title',
+            description: ' ',
+        }
+    },
+    plugins: [
+        searchPlugin({
+            locales:{
+                '/':{
+                    placeholder: 'Search',
+                },
+            }
+        })
+    ],
+    theme: defaultTheme({
+        navbar: [
+            {
+                text: 'Home',
+                link: '/',
+            },
+            {
+                text: 'Bio',
+                link: '/bio.html',
+            },
+            {
+                text: 'Blog',
+                link: '/post',
+            },
+        ],
+        sidebar:[
+            {
+                text: 'Blog',
+                link: '/post',
+            },
+        ],
+    }),
+})
---
+++ docs/bio.md
@@ -0,0 +1,7 @@
+---
+sidebar: false
+---
+
+## About me
+### rrrrrrryo
+田舎（長野県）に住んでいるエンジニアです。  
